a34467d64c1afd7cf3d85627cabbce999d1c6156
Sanstech <ihsanmhd01@gmail.com>
2023-08-03 00:48:01 +0700

    Add Menu

---
+++ admin/modul/laporan/read.php
@@ -0,0 +1,46 @@
+<div class="container-fluid">
+
+  <div class="card card-primary">
+    <div class="card-header">
+      <!-- <h4>Data Laporan</h4> -->
+    </div>
+
+    <div class="card-body">
+      <table id="example1" class="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>Rendering engine</th>
+            <th>Browser</th>
+            <th>Platform(s)</th>
+            <th>Engine version</th>
+            <th>CSS grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Misc</td>
+            <td>PSP browser</td>
+            <td>PSP</td>
+            <td>-</td>
+            <td>C</td>
+          </tr>
+          <tr>
+            <td>Other browsers</td>
+            <td>All others</td>
+            <td>-</td>
+            <td>-</td>
+            <td>U</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+  </div>
+
+</div>
+
+</div>
+
+</div>
+</section>
+</div><!-- /.container-fluid -->
